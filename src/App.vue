393e64fb41adda3d17b604cf8e72b13d3292267d
bobsimonsilalahi <76142877+bobsimonsilalahi@users.noreply.github.com>
2023-04-10 11:37:17 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+ <formInput />
+</template>
+
+<script>
+import formInput from './components/formInput.vue' 
+export default {
+   name: 'App',
+    components: { formInput }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background-image: url('./assets/bg3.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+}
+</style>
